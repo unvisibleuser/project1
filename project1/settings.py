d6b4fe370a2b48ffd09350a69378223258e6887a
smit parmar <parmarsmit376@gmail.com>
2026-09-01 13:54:31 +0530

    added ALLOWED_HOSTS

--- project1/settings.py
+++ project1/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'project1-97wfqptpj-smit-parmar-s-projects.vercel.app/',
+]
 
 
 # Application definition
