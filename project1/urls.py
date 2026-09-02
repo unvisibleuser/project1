b5a168c0567f8095ff2e6baf602ca0e15c5e1a52
smit parmar <parmarsmit376@gmail.com>
2026-09-02 22:45:09 +0530

    static files issue resolved

--- project1/urls.py
+++ project1/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static'))
